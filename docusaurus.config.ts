709184cc27d70cedba1225c574880494f460bfe2
Guillaume Daridon <guillaume.daridon@laposte.net>
2025-12-03 17:16:07 +0100

    upgrade docusaurus

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,7 +20,12 @@ const config: Config  = {
   projectName: 'planetes', // Usually your repo name.
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+  },
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
